a3e06ed91a635f18962847b78dda9f3fb8bcf2c6
Rita-Almeida-lab <82667327+Rita-Almeida-lab@users.noreply.github.com>
2021-04-16 17:57:07 +0100

    Update BackgroundGeolocationHeadlessTask.java

--- src/android/BackgroundGeolocationHeadlessTask.java
+++ src/android/BackgroundGeolocationHeadlessTask.java
@@ -1,66 +1,40 @@
 package com.transistorsoft.cordova.bggeo;
 
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
+import android.content.Context;
+import android.util.Log;
 import org.json.JSONObject;
-
 import com.transistorsoft.locationmanager.adapter.BackgroundGeolocation;
-import com.transistorsoft.locationmanager.event.ActivityChangeEvent;
-import com.transistorsoft.locationmanager.event.ConnectivityChangeEvent;
-import com.transistorsoft.locationmanager.event.GeofenceEvent;
-import com.transistorsoft.locationmanager.event.HeadlessEvent;
-import com.transistorsoft.locationmanager.event.HeartbeatEvent;
-import com.transistorsoft.locationmanager.event.MotionChangeEvent;
-import com.transistorsoft.locationmanager.event.LocationProviderChangeEvent;
-import com.transistorsoft.locationmanager.http.HttpResponse;
-import com.transistorsoft.locationmanager.location.TSLocation;
-import com.transistorsoft.locationmanager.logger.TSLog;
-
 
 /**
  * BackgroundGeolocationHeadlessTask
- * This component allows you to receive events from the BackgroundGeolocation plugin in the native Android environment while your app has been *terminated*,
- * where the plugin is configured for stopOnTerminate: false.  In this context, only the plugin's service is running.  This component will receive all the same
- * events you'd listen to in the Javascript API.
- *
- * You might use this component to:
- * - fetch / post information to your server (eg: request new API key)
- * - execute BackgroundGeolocation API methods (eg: #getCurrentPosition, #setConfig, #addGeofence, #stop, etc -- you can execute ANY method of the Javascript API)
+ * This component allows you to receive events from the BackgroundGeolocation plugin 
+ * in the native Android environment while your app has been *terminated*,
  */
+public class BackgroundGeolocationHeadlessTask extends HeadlessTask implements HeadlessTask.Receiver {
+    /**
+     * @param context
+     * @param event [location|motionchange|providerchange|activitychange|http|heartbeat|geofence|schedule|boot|terminate
+     * @param params Same params signtature provived to Javascript events.
+     */
+    @Override
+    public void onReceive(Context context, String event, JSONObject params) {
+        Log.d("MyApp", "BackgroundGeolocationHeadlessTask: " + params.toString());
 
-public class BackgroundGeolocationHeadlessTask  {
+        // You can get a reference to the BackgroundGeolocation native API like this:
+        BackgroundGeolocation bgGeo = BackgroundGeolocation.getInstance(context);
 
-    @Subscribe(threadMode=ThreadMode.MAIN)
-    public void onHeadlessTask(HeadlessEvent event) {
-        String name = event.getName();
-        TSLog.logger.debug("\uD83D\uDC80  event: " + event.getName());
-        TSLog.logger.debug("- event: " + event.getEvent());
-
-        if (name.equals(BackgroundGeolocation.EVENT_TERMINATE)) {
-            JSONObject state = event.getTerminateEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_LOCATION)) {
-            TSLocation location = event.getLocationEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_MOTIONCHANGE)) {
-            MotionChangeEvent motionChangeEvent = event.getMotionChangeEvent();
-            TSLocation location = motionChangeEvent.getLocation();
-        } else if (name.equals(BackgroundGeolocation.EVENT_HTTP)) {
-            HttpResponse response = event.getHttpEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_PROVIDERCHANGE)) {
-            LocationProviderChangeEvent providerChange = event.getProviderChangeEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_PROVIDERCHANGE)) {
-            LocationProviderChangeEvent providerChange = event.getProviderChangeEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_ACTIVITYCHANGE)) {
-            ActivityChangeEvent activityChange = event.getActivityChangeEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_SCHEDULE)) {
-            JSONObject state = event.getScheduleEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_BOOT)) {
-            JSONObject state = event.getBootEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_GEOFENCE)) {
-            GeofenceEvent geofenceEvent = event.getGeofenceEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_HEARTBEAT)) {
-            HeartbeatEvent heartbeatEvent = event.getHeartbeatEvent(
-			TSCurrentPositionRequest request = new TSCurrentPositionRequest.Builder(event.getContext())
+        // Create custom logic based upon the received event
+        if (event.equals(BackgroundGeolocation.EVENT_HEARTBEAT)) {
+            // A heartbeat event has been received.
+		HeadlessTask.postNotification(context, event, params);
+        } else if (event.equals(BackgroundGeolocation.EVENT_LOCATION)) {
+            // A location event has been received.
+        }
+        // Important: Be sure to execute #finish when your task is complete.  
+        // This signals the native code that your task is complete.
+        finish();
+    }
+	TSCurrentPositionRequest request = new TSCurrentPositionRequest.Builder(event.getContext())
                     .setPersist(true)       // <-- yes, persist to database
                     .setSamples(1)          // <-- fetch 1 location sample
                     .setCallback(new TSLocationCallback() {
@@ -77,15 +51,4 @@ public class BackgroundGeolocationHeadlessTask  {
 
                 BackgroundGeolocation bgGeo = BackgroundGeolocation.getInstance(event.getContext());
                 bgGeo.getCurrentPosition(request);
-		    );
-        } else if (name.equals(BackgroundGeolocation.EVENT_ENABLEDCHANGE)) {
-            boolean enabled = event.getEnabledChangeEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_CONNECTIVITYCHANGE)) {
-            ConnectivityChangeEvent connectivityChangeEvent = event.getConnectivityChangeEvent();
-        } else if (name.equals(BackgroundGeolocation.EVENT_POWERSAVECHANGE)) {
-            boolean powerSaveEnabled = event.getPowerSaveChangeEvent().isPowerSaveMode();
-        } else {
-            TSLog.logger.warn(TSLog.warn("Unknown Headless Event: " + name));
-        }
-    }
 }
