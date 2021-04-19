cce2ae102ca4cd7734a72f5a74f909512037a4f9
Rita-Almeida-lab <82667327+Rita-Almeida-lab@users.noreply.github.com>
2021-04-19 13:53:34 +0100

    Update BackgroundGeolocationHeadlessTask.java

--- src/android/BackgroundGeolocationHeadlessTask.java
+++ src/android/BackgroundGeolocationHeadlessTask.java
@@ -19,52 +19,29 @@ import com.transistorsoft.locationmanager.location.TSCurrentPositionRequest;
 import com.transistorsoft.locationmanager.location.TSLocation;
 import com.transistorsoft.locationmanager.logger.TSLog;
 
-public class BackgroundGeolocationHeadlessTask  {
+public class BackgroundGeolocationHeadlessTask extends HeadlessTask implements HeadlessTask.Receiver {
+    /**
+     * @param context
+     * @param event [location|motionchange|providerchange|activitychange|http|heartbeat|geofence|schedule|boot|terminate
+     * @param params Same params signtature provived to Javascript events.
+     */
+    @Override
+    public void onReceive(Context context, String event, JSONObject params) {
+        Log.d("MyApp", "BackgroundGeolocationHeadlessTask: " + params.toString());
 
-    @Subscribe
-    public void onHeadlessTask(HeadlessEvent event) {
+        // You can get a reference to the BackgroundGeolocation native API like this:
+        BackgroundGeolocation bgGeo = BackgroundGeolocation.getInstance(context);
 
-        String name = event.getName();
-        TSLog.logger.debug("\uD83D\uDC80  event (CUSTOM IMPLEMENTATION): " + event.getName());
-        TSLog.logger.debug("- event: " + event.getEvent());
-        
-        // Get reference to plugin singleton
-           BackgroundGeolocation bgGeo = BackgroundGeolocation.getInstance(context);
-
-        if (name.equals(BackgroundGeolocation.EVENT_HEARTBEAT)) {
-            // [Optional] Build extras object.
-            JSONObject extras = new JSONObject();
-            try {
-                extras.put("foo", "bar");
-            } catch (JSONException e) {
-                // Won't happen.
-            }
-
-            // Build request object.
-            TSCurrentPositionRequest.Builder request = new TSCurrentPositionRequest.Builder(event.getContext())
-                .setPersist(true)  // <-- optional
-                .setMaximumAge(0)  // <-- optional
-                .setExtras(extras) // <-- optional 
-                .setTimeout(60)    // <-- optional
-                .setSamples(3)     // <-- optional 
-                //.setDesiredAccuracy(10)  // <-- optional
-                .setCallback(new TSLocationCallback() {
-                    @Override
-                    public void onLocation(TSLocation tsLocation) {
-                        // Location received callback.
-                        TSLog.logger.debug("*** Location received: " + tsLocation.toString());      
-                    }
-                    @Override
-                    public void onError(Integer error) {
-                        // Location error callback.
-                        TSLog.logger.error("*** getCurrentPosition ERROR: " + error.toString());
-                    }
-                });
-
-            // Initiate the request.
-            BackgroundGeolocation.getInstance(event.getContext()).getCurrentPosition(request.build());  
-
-            HeartbeatEvent heartbeatEvent = event.getHeartbeatEvent();
+        // Create custom logic based upon the received event
+        if (event.equals(BackgroundGeolocation.EVENT_HEARTBEAT)) {
+            // A heartbeat event has been received.
+			HeadlessTask.postNotification(context, event, params);
+			alert("in background");
+        } else if (event.equals(BackgroundGeolocation.EVENT_LOCATION)) {
+            // A location event has been received.
         }
+        // Important: Be sure to execute #finish when your task is complete.  
+        // This signals the native code that your task is complete.
+        finish();
     }
 }
